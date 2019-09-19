cdf14df09b821e5d2cefb2227d9af17a8cdb817e
MhmdMahmoud <mohammad.ok1@outlook.com>
2019-09-19 16:04:53 +0200

    finishing main v1

--- app/src/main/java/com/example/newz/MainActivity.java
+++ app/src/main/java/com/example/newz/MainActivity.java
@@ -2,12 +2,72 @@ package com.example.newz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import com.example.newz.API.ApiClient;
+import com.example.newz.API.INewsApi;
+import com.example.newz.Model.Article;
+import com.example.newz.Model.News;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Article> articles = new ArrayList<>();
+    private NewsAdapter newsAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.main_recycler);
+        layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setNestedScrollingEnabled(false);
+
+        loadNews();
+    }
+
+    public void loadNews(){
+
+        INewsApi newsApi = ApiClient.getApiClient().create(INewsApi.class);
+
+        String country = Utils.getCountry();
+
+        Call<News> call;
+        call = newsApi.getNews(country, Const.API_KEY);
+        call.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if(response.isSuccessful() && response.body().getArticles() != null){
+                    if (!articles.isEmpty()){
+                        articles.clear();
+                    }
+                    articles = response.body().getArticles();
+                    newsAdapter = new NewsAdapter(articles, MainActivity.this);
+                    recyclerView.setAdapter(newsAdapter);
+                    newsAdapter.notifyDataSetChanged();
+
+                }else {
+                    Toast.makeText(MainActivity.this,"no news", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/newz/NewsAdapter.java
+++ app/src/main/java/com/example/newz/NewsAdapter.java
@@ -1,11 +1,23 @@
 package com.example.newz;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.example.newz.Model.Article;
 
 import java.util.List;
@@ -13,37 +25,83 @@ import java.util.List;
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.MyViewHolder> {
     private List<Article> articles;
     Context context;
+    private OnItemClickListener onItemClickListener;
 
     public NewsAdapter(List<Article> articles, Context context) {
         this.articles = articles;
         this.context = context;
     }
-    private OnItemClickListener onItemClickListener;
-
 
     @NonNull
     @Override
-    public NewsAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.news_item, viewGroup, false);
+        return new MyViewHolder(view, onItemClickListener);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull NewsAdapter.MyViewHolder myViewHolder, int i) {
+    public void onBindViewHolder(@NonNull final MyViewHolder myViewHolder, int i) {
 
+        Article model = articles.get(i);
+
+        Glide.with(context)
+                .load(model.getUrlToImage())
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        myViewHolder.progress.setVisibility(View.GONE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        myViewHolder.progress.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(myViewHolder.news_image);
+
+        myViewHolder.title.setText(model.getTitle());
+        myViewHolder.author.setText(model.getAuthor());
+        myViewHolder.description.setText(model.getDescription());
+        myViewHolder.source.setText(model.getSource().getName());
+        myViewHolder.time.setText(Utils.DateToTimeFormat(model.getPublishedAt()));
+        myViewHolder.published_at.setText(Utils.DateFormat(model.getPublishedAt()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return articles.size();
     }
 
     public interface OnItemClickListener{
         void onItemClick(View view, int Position);
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        public MyViewHolder(@NonNull View itemView) {
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView author, title, published_at, description, source, time;
+        ImageView news_image;
+        ProgressBar progress;
+        OnItemClickListener onItemClickListener;
+        public MyViewHolder(@NonNull View itemView, OnItemClickListener onItemClickListener) {
             super(itemView);
+            itemView.setOnClickListener(this);
+            title = itemView.findViewById(R.id.title);
+            author = itemView.findViewById(R.id.author);
+            description = itemView.findViewById(R.id.description);
+            source = itemView.findViewById(R.id.source);
+            time = itemView.findViewById(R.id.time);
+            published_at = itemView.findViewById(R.id.publishedAt);
+            news_image = itemView.findViewById(R.id.news_img);
+            progress = itemView.findViewById(R.id.progress_load_photo);
+
+            this.onItemClickListener = onItemClickListener;
+        }
+
+        @Override
+        public void onClick(View v) {
+
         }
     }
 }
